9d976ad7de659c3693fbd7419c0dfbb6364c3c64
Iurii Kozuliak <i.kozuliak@gmail.com>
2013-08-15 00:14:24 +0200

    Initial text particles generation

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -32,7 +32,7 @@
                 FPS:60,
                 words:[ 'cola zero' ],
                 colors:{
-                    circle:[ '#e67e22', '#2c3e50' ]
+                    circle:[ '#ed172a', '#fafafa' ]
                 }
             }
         },
@@ -73,7 +73,7 @@
 
             var self = this;
 
-            for(var quantity = 0, len = 100; quantity < len; quantity++) {
+            for (var quantity = 0, len = 100; quantity < len; quantity++) {
 
                 var x, y, steps,
 
@@ -89,56 +89,124 @@
 
                 this.particles.push({
 
-                    x: x,
-                    y: y,
+                    x:x,
+                    y:y,
 
-                    hasBorn: hasBorn,
+                    hasBorn:hasBorn,
 
-                    ease: 0.04 + Math.random() * 0.06,
-                    bornSpeed: 0.07 + Math.random() * 0.07,
+                    ease:0.04 + Math.random() * 0.06,
+                    bornSpeed:0.07 + Math.random() * 0.07,
 
-                    alpha: 0,
-                    maxAlpha: 0.7 + Math.random() * 0.4,
+                    alpha:0,
+                    maxAlpha:0.7 + Math.random() * 0.4,
 
-                    radius: radius,
-                    maxRadius: 12,
+                    radius:radius,
+                    maxRadius:12,
 
-                    color: color,
-                    interactive: false,
+                    color:color,
+                    interactive:false,
 
-                    angle: 0,
+                    angle:0,
 
-                    steps: steps
+                    steps:steps
 
                 });
 
             }
 
+
+            this._createTextParticles();
             this._renderLoop();
 
         },
-        
 
-        _renderLoop : function(){
+        _generateText : function(){
+            this.context.font = 100 + 'px Arial, sans-serif';
+            this.context.fillStyle = 'rgb(255, 255, 255)';
+            this.context.textAlign = 'center';
 
-            this._clear();
-            this._update();
-            this._render();
-            requestAnimFrame(this._renderLoop.bind(this));
+            var sentence = this.options.words[0].toUpperCase().split('').join(String.fromCharCode(8202));
+
+            this.context.fillText(sentence, this.canvas.width * 0.5, this.canvas.height - 50);
+        },
+
+        _grabParticlesFromCanvas:function () {
+
+            var surface = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
+
+            for (var width = 0; width < surface.width; width += 4) {
+
+                for (var height = 0; height < surface.height; height += 4) {
+
+                    var color = surface.data[(height * surface.width * 4) + (width * 4) - 1];
+
+                    if (color === 255) {
+                        this.nextText.push({
+                            x:width,
+                            y:height,
+                            orbit:randomBetween(1, 3),
+                            angle:0
+                        });
+
+                    }
+                }
+
+            }
+
+            return this.nextText.length;
 
         },
 
-        _clear : function(){
+        _createTextParticles:function (seed) {
+
+            this._generateText();
+            var seed = this._grabParticlesFromCanvas();
+
+            for (var quantity = 0, len = seed; quantity < len; quantity++) {
+
+                var radius = randomBetween(0, 12);
+                var hasBorn = radius > 0 || radius < 12 ? false : true;
+
+                var color = this.options.colors.circle[~~(Math.random() * this.options.colors.circle.length)];
+
+                this.text.push({
+
+                    x:this.canvas.width * 0.5,
+                    y:this.canvas.height - 70,
+
+                    hasBorn:hasBorn,
+
+                    ease:0.04 + Math.random() * 0.06,
+                    bornSpeed:0.07 + Math.random() * 0.07,
+
+                    alpha:0,
+                    maxAlpha:0.7 + Math.random() * 0.4,
+
+                    radius:radius,
+                    maxRadius:4,
+
+                    color:color,
+                    interactive:false
+
+                });
+
+            }
+
+
+        },
+
+
+        _clear:function () {
             this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         },
 
-        _updateTransition: function(){
+        _updateTransition:function () {
 
             var self = this;
 
-            [].forEach.call(this.particles, function(particle, index) {
+            [].forEach.call(this.particles, function (particle, index) {
 
-                switch(self.options.currentTransition) {
+                switch (self.options.currentTransition) {
 
                     case 'circle':
 
@@ -147,11 +215,12 @@
 
                         break;
 
-                    default: break;
+                    default:
+                        break;
 
                 }
 
-                if(!particle.interactive) {
+                if (!particle.interactive) {
 
                     particle.x += ((self.shape.x + Math.cos(particle.angle) * 5) - particle.x) * 0.08;
                     particle.y += ((self.shape.y + Math.sin(particle.angle) * 5) - particle.y) * 0.08;
@@ -167,27 +236,49 @@
 
                 particle.angle += 0.08;
 
-            })
+            });
+
+            [].forEach.call(this.nextText, function (particle, index) {
+
+                if (!self.text[index].interactive) {
+
+                    self.text[index].x += ((particle.x + Math.cos(particle.angle + index) * particle.orbit) - self.text[index].x) * 0.08;
+                    self.text[index].y += ((particle.y + Math.sin(particle.angle + index) * particle.orbit) - self.text[index].y) * 0.08;
+
+                }
+
+                else {
+
+                    self.text[index].x += ((mouse.x + Math.sin(particle.angle) * 30) - self.text[index].x) * 0.08;
+                    self.text[index].y += ((mouse.y + Math.cos(particle.angle) * 30) - self.text[index].y) * 0.08;
+
+                }
+
+                particle.angle += 0.08;
+
+            });
+
         },
 
-        _update : function(){
+
+        _update:function () {
 
             var self = this;
 
             this._updateTransition();
 
 
-            [].forEach.call(this.particles, function(particle, index) {
+            [].forEach.call(this.particles, function (particle, index) {
 
                 particle.alpha += (particle.maxAlpha - particle.alpha) * 0.05;
 
-                if(particle.hasBorn) {
+                if (particle.hasBorn) {
 
                     particle.radius += (0 - particle.radius) * particle.bornSpeed;
 
-                    if(Math.round(particle.radius) === 0) {
+                    if (Math.round(particle.radius) === 0) {
 
-                        switch(self.options.currentTransition) {
+                        switch (self.options.currentTransition) {
 
                             case 'circle':
 
@@ -195,13 +286,40 @@
 
                                 break;
 
-                            default: break;
+                            default:
+                                break;
 
                         }
 
                         particle.hasBorn = false;
 
                     }
+
+                }
+
+                if (!particle.hasBorn) {
+
+                    particle.radius += (particle.maxRadius - particle.radius) * particle.bornSpeed;
+
+                    if (Math.round(particle.radius) === particle.maxRadius)
+
+                        particle.hasBorn = true;
+
+                }
+
+            });
+
+            [].forEach.call(this.text, function(particle, index) {
+
+                particle.alpha += (particle.maxAlpha - particle.alpha) * 0.05;
+
+                if(particle.hasBorn) {
+
+                    particle.radius += (0 - particle.radius) * particle.bornSpeed;
+
+                    if(Math.round(particle.radius) === 0)
+
+                        particle.hasBorn = false;
 
                 }
 
@@ -220,11 +338,11 @@
 
         },
 
-        _render : function() {
+        _render:function () {
 
             var context = this.context;
 
-            [].forEach.call(this.particles, function(particle, index) {
+            [].forEach.call(this.particles, function (particle, index) {
 
                 context.save();
                 context.globalAlpha = particle.alpha;
@@ -236,7 +354,28 @@
 
             });
 
-        }
+            [].forEach.call(this.text, function(particle, index) {
+
+                context.save();
+                context.globalAlpha = particle.alpha;
+                context.fillStyle = 'rgb(255, 255, 255)';
+                context.beginPath();
+                context.arc(particle.x, particle.y, particle.radius, 0, Math.PI * 2);
+                context.fill();
+                context.restore();
+
+            });
+
+        },
+
+        _renderLoop:function () {
+
+            this._clear();
+            this._update();
+            this._render();
+            requestAnimFrame(this._renderLoop.bind(this));
+
+        },
 
 
     }
